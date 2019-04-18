9d1c4a1881e7e2e54f9b078afc2794d9b24fb016
Ruslan <razinurov@gmail.com>
2019-04-18 23:03:50 +0300

    Новый видос + свгшная кнопка

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <a class="mailto" target="_blank" href="mailto:info@rms.group">
+          <img src="./img/Group.svg" />
+        </a>
         <video autoPlay muted playsInline loop id="rms">
-          <source src="/video/RMS.mp4" type="video/mp4"/>
+          <source src="./video/RMS.mp4" type="video/mp4"/>
           <p>Your browser doesn't support HTML5 video. Here is
-            a <a href="/video/RMS.mp4">link to the video</a> instead.</p>
+            a <a href="./video/RMS.mp4">link to the video</a> instead.</p>
         </video>
+        <div className="heh">Согласны? Узнали?</div>
       </div>
     );
   }
