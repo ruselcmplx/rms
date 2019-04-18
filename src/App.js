ae0c16c6e148985e9d2ab74959c7edaa1f890be5
Ruslan <razinurov@gmail.com>
2019-04-18 21:59:33 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <video autoPlay muted playsInline loop id="rms">
+          <source src="/video/RMS.mp4" type="video/mp4"/>
+          <p>Your browser doesn't support HTML5 video. Here is
+            a <a href="/video/RMS.mp4">link to the video</a> instead.</p>
+        </video>
       </div>
     );
   }
