2e5ad82c576530242e00ce955c864c8ca7cc2ffd
Ruslan <razinurov@gmail.com>
2019-04-18 22:01:11 +0300

    Updates

---
+++ precache-manifest.5bd6ad74905f85dd75d98cf514c35ca2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "649bfeb50f421a898031",
+    "url": "/rms/static/js/runtime~main.06524e64.js"
+  },
+  {
+    "revision": "15f15c00202137a8cfdd",
+    "url": "/rms/static/js/main.043930f6.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/rms/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "15f15c00202137a8cfdd",
+    "url": "/rms/static/css/main.ae8c1518.chunk.css"
+  },
+  {
+    "revision": "623e16107a76ab281f9e0cdc4cb0355e",
+    "url": "/rms/index.html"
+  }
+];
