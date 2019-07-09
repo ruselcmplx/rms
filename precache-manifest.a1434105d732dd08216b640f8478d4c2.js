b2758f3ea7dce97d316a2490f30dc01bd7898dfe
ra.zinurov <razinurov@gmail.com>
2019-07-09 22:44:48 +0300

    Updates

---
+++ precache-manifest.a1434105d732dd08216b640f8478d4c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a895087708940c3af7cf0c96dafdfb1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3c3242438ad23ec9da7",
+    "url": "/static/css/main.fffec52a.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "d3c3242438ad23ec9da7",
+    "url": "/static/js/main.91a48baa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5dabd32d1a12f2e06d29410cc7bf785b",
+    "url": "/static/media/Inter-Regular.5dabd32d.woff"
+  },
+  {
+    "revision": "6e1bdd3689fad8d63f6d17c89fdeb3b1",
+    "url": "/static/media/Inter-Regular.6e1bdd36.ttf"
+  }
+]);
