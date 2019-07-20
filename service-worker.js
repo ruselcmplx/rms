c2b5de6152f89962a684d53d8a192c1d229a44fe
ra.zinurov <razinurov@gmail.com>
2019-07-20 19:26:05 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a1434105d732dd08216b640f8478d4c2.js"
+  "/precache-manifest.d9928ab0d985d7f93dca71f31747b0e9.js"
 );
 
 self.addEventListener('message', (event) => {
